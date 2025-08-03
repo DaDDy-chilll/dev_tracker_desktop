d644cc85b885b6cb70611655038a5180e75f09a8
DaDDy-Chill <blacklistuser420@gmail.com>
2025-08-03 19:36:22 +0630

    refactor: relocate Skyer font file and update import paths

--- src/renderer/src/assets/fonts.ts
+++ src/renderer/src/assets/fonts.ts
@@ -1,5 +1,5 @@
 // Import font files directly as modules
-import skyerFontUrl from '../../fonts/skyer-400.otf'
+import skyerFontUrl from '../../../fonts/skyer-400.otf'
 
 // Export font URLs for use in CSS
 export const fonts = {
